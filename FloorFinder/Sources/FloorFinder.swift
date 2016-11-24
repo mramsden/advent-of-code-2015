b4feba98744714acd80a97f47db63a9373ef71f0
Marcus Ramsden <marcus.ramsden@gmail.com>
2016-11-24 23:05:48 +0000

    Implement the simplest case for TDD

--- FloorFinder/Sources/FloorFinder.swift
+++ FloorFinder/Sources/FloorFinder.swift
@@ -2,10 +2,8 @@ import Foundation
 
 struct FloorFinder {
   func findFloor(_ specification: String) -> Int {
-    return specification.characters.reduce(0) { sum, character in
-      if character == "(" { return sum + 1 }
-      if character == ")" { return sum - 1 }
-      return sum
-    }
+    if specification == "(" { return 1 }
+    if specification == ")" { return -1 }
+    return 0
   }
 }
