f1d6737b975e4c14991cf41630bde27771c30b92
Marcus Ramsden <marcus.ramsden@moonpig.com>
2016-11-25 10:01:22 +0000

    Refactor the public API

--- WrappingPaperCalculator/Sources/WrappingPaperCalculator.swift
+++ WrappingPaperCalculator/Sources/WrappingPaperCalculator.swift
@@ -5,15 +5,6 @@ enum WrappingPaperCalculatorError: Error {
 }
 
 struct WrappingPaperCalculator {
-  func parseComponents(_ input: String) throws -> (Int, Int, Int) {
-    var components = input.components(separatedBy: "x").flatMap { Int($0) }
-    if components.count < 3 || components.count > 3 {
-      throw WrappingPaperCalculatorError.invalidFormat
-    }
-
-    return (components[0], components[1], components[2])
-  }
-
   func areaRequired(specification input: String) throws -> Int {
     let components = try parseComponents(input)
     return areaRequired(components.0, components.1, components.2)
@@ -22,5 +13,14 @@ struct WrappingPaperCalculator {
   func areaRequired(_ l: Int, _ w: Int, _ h: Int) -> Int {
     let sides = [l * w, w * h, h * l]
     return sides.map{ $0 * 2 }.reduce(0, +) + (sides.min() ?? 0)
+  }
+
+  private func parseComponents(_ input: String) throws -> (Int, Int, Int) {
+    var components = input.components(separatedBy: "x").flatMap { Int($0) }
+    if components.count < 3 || components.count > 3 {
+      throw WrappingPaperCalculatorError.invalidFormat
+    }
+
+    return (components[0], components[1], components[2])
   }
 }
--- WrappingPaperCalculator/Tests/WrappingPaperCalculatorTests/WrappingPaperCalculatorTests.swift
+++ WrappingPaperCalculator/Tests/WrappingPaperCalculatorTests/WrappingPaperCalculatorTests.swift
@@ -2,26 +2,6 @@ import XCTest
 @testable import WrappingPaperCalculator
 
 class WrappingPaperCalculatorTests: XCTestCase {
-    func testCanExtractWidthHeightAndLengthComponents() {
-      let calculator = WrappingPaperCalculator()
-      do {
-        let components1 = try calculator.parseComponents("2x3x4")
-        let components2 = try calculator.parseComponents("1x1x10")
-        XCTAssert((2, 3, 4) == components1)
-        XCTAssert((1, 1, 10) == components2)
-      } catch {
-        XCTFail("Unexpected error")
-      }
-    }
-
-    func testThrowsErrorForUnexpectedInput() {
-      let calculator = WrappingPaperCalculator()
-      do {
-        let _ = try calculator.parseComponents("lxwxh")
-        XCTFail("Expected invalidFormat error")
-      } catch {}
-    }
-
     func testCalculatesWrappingPaperAreaRequired() {
       let calculator = WrappingPaperCalculator()
       let areaRequired1 = calculator.areaRequired(2, 3, 4)
@@ -40,6 +20,14 @@ class WrappingPaperCalculatorTests: XCTestCase {
       } catch {
         XCTFail("Unexpected error")
       }
+    }
+
+    func testThrowsErrorForUnexpectedInput() {
+      let calculator = WrappingPaperCalculator()
+      do {
+        let _ = try calculator.areaRequired(specification: "lxwxh")
+        XCTFail("Expected invalidFormat error")
+      } catch {}
     }
 }
 
