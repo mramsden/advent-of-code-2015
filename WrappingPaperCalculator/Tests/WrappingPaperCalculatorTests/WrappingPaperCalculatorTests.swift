a6e0f48e7b50a05687f71a8f3071e4cfad898b22
Marcus Ramsden <marcus.ramsden@gmail.com>
2016-11-25 07:37:40 +0000

    Verify unexpected format is caught

--- WrappingPaperCalculator/Tests/WrappingPaperCalculatorTests/WrappingPaperCalculatorTests.swift
+++ WrappingPaperCalculator/Tests/WrappingPaperCalculatorTests/WrappingPaperCalculatorTests.swift
@@ -5,11 +5,21 @@ class WrappingPaperCalculatorTests: XCTestCase {
     func testCanExtractWidthHeightAndLengthComponents() {
       let calculator = WrappingPaperCalculator()
       do {
-        XCTAssert((2, 3, 4) == (try calculator.parseComponents("2x3x4")))
-        XCTAssert((1, 1, 10) == (try calculator.parseComponents("1x1x10")))
-      } catch let error {
-        XCTFail("Unexpected error \(error)")
+        let components1 = try calculator.parseComponents("2x3x4")
+        let components2 = try calculator.parseComponents("1x1x10")
+        XCTAssert((2, 3, 4) == components1)
+        XCTAssert((1, 1, 10) == components2)
+      } catch {
+        XCTFail("Unexpected error")
       }
+    }
+
+    func testThrowsErrorForUnexpectedInput() {
+      let calculator = WrappingPaperCalculator()
+      do {
+        let _ = try calculator.parseComponents("lxwxh")
+        XCTFail("Expected invalidFormat error")
+      } catch {}
     }
 }
 
