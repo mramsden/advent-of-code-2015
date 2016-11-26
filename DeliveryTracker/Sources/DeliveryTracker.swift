656a1a4add50bf8247c48ca526e53b711f5d1ae2
Marcus Ramsden <marcus.ramsden@gmail.com>
2016-11-26 11:42:31 +0000

    Add delivery tracker movement

--- DeliveryTracker/Sources/DeliveryTracker.swift
+++ DeliveryTracker/Sources/DeliveryTracker.swift
@@ -1,7 +1,23 @@
 typealias DeliveryCoordinate = (x: Int, y: Int)
 
+enum Direction {
+  case north
+  case east
+  case south
+  case west
+}
+
 class DeliveryTracker {
 
     private(set) var currentLocation = (x: 0, y: 0)
     private(set) var numberOfHousesVisited = 1
+
+    func move(_ direction: Direction) {
+      switch direction {
+        case .north: currentLocation.y += 1
+        case .east: currentLocation.x += 1
+        case .south: currentLocation.y -= 1
+        case .west: currentLocation.x -= 1
+      }
+    }
 }
--- DeliveryTracker/Tests/DeliveryTrackerTests/DeliveryTrackerTests.swift
+++ DeliveryTracker/Tests/DeliveryTrackerTests/DeliveryTrackerTests.swift
@@ -12,6 +12,34 @@ class DeliveryTrackerTests: XCTestCase {
       let deliveryTracker = DeliveryTracker()
       XCTAssertEqual(1, deliveryTracker.numberOfHousesVisited)
     }
+
+    func testCanMoveNorth() {
+      let deliveryTracker = DeliveryTracker()
+      deliveryTracker.move(.north)
+      XCTAssertEqual(0, deliveryTracker.currentLocation.x)
+      XCTAssertEqual(1, deliveryTracker.currentLocation.y)
+    }
+
+    func testCanMoveEast() {
+      let deliveryTracker = DeliveryTracker()
+      deliveryTracker.move(.east)
+      XCTAssertEqual(1, deliveryTracker.currentLocation.x)
+      XCTAssertEqual(0, deliveryTracker.currentLocation.y)
+    }
+
+    func testCanMoveSouth() {
+      let deliveryTracker = DeliveryTracker()
+      deliveryTracker.move(.south)
+      XCTAssertEqual(0, deliveryTracker.currentLocation.x)
+      XCTAssertEqual(-1, deliveryTracker.currentLocation.y)
+    }
+
+    func testCanMoveWest() {
+      let deliveryTracker = DeliveryTracker()
+      deliveryTracker.move(.west)
+      XCTAssertEqual(-1, deliveryTracker.currentLocation.x)
+      XCTAssertEqual(0, deliveryTracker.currentLocation.y)
+    }
 }
 
 #if os(Linux)
@@ -20,6 +48,10 @@ extension DeliveryTrackerTests {
     return [
       ("testStartsAtOriginCurrentLocation", testStartsAtOriginCurrentLocation),
       ("testStartsWithAtLeastOneHouseVisited", testStartsWithAtLeastOneHouseVisited),
+      ("testCanMoveNorth", testCanMoveNorth),
+      ("testCanMoveEast", testCanMoveEast),
+      ("testCanMoveSouth", testCanMoveSouth),
+      ("testCanMoveWest", testCanMoveWest),
     ]
   }
 }
