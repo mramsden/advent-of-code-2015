1b97fc4b31a958d3721d95767b4b1b03dc81f291
Marcus Ramsden <marcus.ramsden@gmail.com>
2016-11-27 09:35:01 +0000

    Detect repeated characters in string

--- NiceStringDetector/Sources/NiceStringDetector.swift
+++ NiceStringDetector/Sources/NiceStringDetector.swift
@@ -2,10 +2,26 @@ struct NiceStringDetector {
   static let vowels = "aeiou"
 
   func isNice(string: String) -> Bool {
+    return vowelsValid(string: string) &&
+      repeatsCharacters(string: string)
+  }
+
+  private func vowelsValid(string: String) -> Bool {
     return string.characters.filter { character in
       return NiceStringDetector.vowels.characters.filter { vowel in
         return character == vowel
       }.count >= 1
     }.count >= 3
+  }
+
+  private func repeatsCharacters(string: String) -> Bool {
+    var lastCharacter: Character? = nil
+    return string.characters.reduce(false) { repeats, character in
+      if lastCharacter == nil || lastCharacter != character {
+        lastCharacter = character
+        return repeats
+      }
+      return true
+    }
   }
 }
--- NiceStringDetector/Tests/NiceStringDetectorTests/NiceStringDetectorTests.swift
+++ NiceStringDetector/Tests/NiceStringDetectorTests/NiceStringDetectorTests.swift
@@ -10,11 +10,19 @@ class NiceStringDetectorTests: XCTestCase {
   }
 
   func testStringWithThreeOrMoreVowelsIsNice() {
-    XCTAssertTrue(detector.isNice(string: "nice string"))
+    XCTAssertTrue(detector.isNice(string: "nicce string"))
   }
 
   func testStringWithTwoOrFewerVowelsIsNotNice() {
-    XCTAssertFalse(detector.isNice(string: "nice"))
+    XCTAssertFalse(detector.isNice(string: "nicce"))
+  }
+
+  func testStringWithRepeatedLettersIsNice() {
+    XCTAssertTrue(detector.isNice(string: "nicce string"))
+  }
+
+  func testStringWithoutRepeatedLettersIsNotNice() {
+    XCTAssertFalse(detector.isNice(string: "nice string"))
   }
 }
 
@@ -24,6 +32,8 @@ extension NiceStringDetectorTests {
     return [
       ("testStringWithThreeOrMoreVowelsIsNice", testStringWithThreeOrMoreVowelsIsNice),
       ("testStringWithTwoOrFewerVowelsIsNotNice", testStringWithTwoOrFewerVowelsIsNotNice),
+      ("testStringWithRepeatedLettersIsNice", testStringWithRepeatedLettersIsNice),
+      ("testStringWithoutRepeatedLettersIsNotNice", testStringWithoutRepeatedLettersIsNotNice),
     ]
   }
 }
